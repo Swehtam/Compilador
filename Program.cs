990175dea391b921f5fa8ca6142bfa5213289d86
Swehtam <mathewsalves1@outlook.com>
2019-06-22 22:06:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:/Users/mathe/Desktop/teste.txt";
+
+            // Como ler um arquivo linha por linha
+            using (StreamReader file = new StreamReader(path))
+            {
+                int counter = 0;
+                char ln;
+
+                while (file.Peek() >= 0)
+                {
+                    int num_letra = file.Read();
+                    char letra = (char)num_letra;
+                    Console.WriteLine(num_letra + "="+ letra);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} characteres.");
+            }
+        }
+    }
+}
