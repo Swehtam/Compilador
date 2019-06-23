3dff753a0b6fe283d6b0b3c9a6d2a52a962cef1b
Swehtam <mathewsalves1@outlook.com>
2019-06-22 23:00:21 -0300

    second commit

--- Program.cs
+++ Program.cs
@@ -7,23 +7,43 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            string path = @"C:/Users/mathe/Desktop/teste.txt";
+            string readPath = @"..\..\teste.txt";
+            string writePath = @"..\..\writeTeste.txt";
 
             // Como ler um arquivo linha por linha
-            using (StreamReader file = new StreamReader(path))
+            using (StreamWriter writeFile = new StreamWriter(writePath))
             {
-                int counter = 0;
-                char ln;
-
-                while (file.Peek() >= 0)
+                writeFile.WriteLine("TOKEN   |   TIPO    |   LINHA");
+                using (StreamReader readFile = new StreamReader(readPath))
                 {
-                    int num_letra = file.Read();
-                    char letra = (char)num_letra;
-                    Console.WriteLine(num_letra + "="+ letra);
-                    counter++;
+                    int counter = 1;
+                    char ln;
+
+                    while (readFile.Peek() >= 0)
+                    {
+                        bool escreve = true;
+                        int num_letra = readFile.Read();
+                        if(num_letra == 13)
+                        {
+                            counter++;
+                            escreve = false;
+                        }
+                        else if(num_letra == 32 || num_letra == 9 || num_letra == 10)
+                        {
+                            escreve = false;
+                        }
+
+                        if (escreve)
+                        {
+                            char letra = (char)num_letra;
+                            Console.WriteLine(letra + "       |   blabla  |   " + counter);
+                            writeFile.WriteLine(letra + "       |   blabla  |   " + counter);
+                        }
+                    }
+                    readFile.Close();
+                    Console.WriteLine($"File has {counter} lines.");
                 }
-                file.Close();
-                Console.WriteLine($"File has {counter} characteres.");
+                writeFile.Close();
             }
         }
     }
