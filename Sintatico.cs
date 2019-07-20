f3dd494aaa771e8a322be98ecfeb26253b922b95
bivar <rebecabivar@cc.ci.ufpb.br>
2019-07-19 21:41:13 -0300

    Funções básicas implementadas

--- Sintatico.cs
+++ Sintatico.cs
@@ -9,6 +9,7 @@ namespace Teste
     {
         static void Main(string[] args)
         {
+
             // Mudar o path do arquivo que será lido
             string readPath = @"..\..\teste.txt";
             using (StreamReader readFile = new StreamReader(readPath))
@@ -22,6 +23,84 @@ namespace Teste
 
         static void Programa(LexLine obj)
         {
+            if(obj.token == "program")
+            {
+                obj.NextLine(); //Fazer ir para outra linha!!!
+                if(obj.type == "Identificador")
+                {
+                    obj.NextLine();
+                    if(obj.token == ";")
+                    {
+                        obj.NextLine();
+                        DeclaraVariaveis(obj);
+
+                    }
+                }
+            }
+        }
+
+        static void DeclaraVariaveis(LexLine obj)
+        {
+
+        }
+
+        //A gramática chegou em um operador relacional
+        static void Relacional(LexLine obj)
+        {
+            //checa se o token atual é, de fato, um op relacional
+            switch (obj.token)
+            {
+                case "=":
+                    return;
+                case ">":
+                    return;
+                case "<":
+                    return;
+                case ">=":
+                    return;
+                case "<=":
+                    return;
+                case "<>":
+                    return;
+            }
+        }
+
+        //A gramática chegou em um operador aditivo
+        static void Additive(LexLine obj)
+        {
+            //checa se o token atual é, de fato, um op aditivo
+            switch (obj.token)
+            {
+                case "+":
+                    return;
+                case "-":
+                    return;
+                case "or":
+                    return;               
+            }
+        }
+
+        //A gramática chegou em um operador multiplicativo
+        static void Multiplicative(LexLine obj)
+        {
+            //checa se o token atual é, de fato, um op multiplicativo
+            switch (obj.token)
+            {
+                case "*":
+                    return;
+                case "/":
+                    return;
+                case "and":
+                    return;
+            }
+        }
+
+        //Checa se a gramática foi lida com sucesso ou não
+        /******* LEMBRAR DE CONTINUAR DEPOIS ******/
+        static void Sucess(bool succeded)
+        {
+            if (succeded)
+                Console.WriteLine("Gramática lida com sucesso!");
 
         }
     }
